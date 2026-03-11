833bc436c992f519404b94bdabb0366cedd5c29a
parksw <parksw9659@gmail.com>
2026-03-11 16:34:31 +0900

    0311 박상원 app.js api 추가 수정

--- backend/app.js
+++ backend/app.js
@@ -33,7 +33,7 @@ app.get("/", function (req, res, next) {
 });
 
 const Router = require("./router/router.js"); // 모든 라우터가 모여있는 파일이 router.js
-app.use("/", Router); // 수정
+app.use("/api", Router); // 수정
 
 app.use((req, res) => {
   res.status(404).sendFile(path.join(__dirname, "./public", "index.html"));
